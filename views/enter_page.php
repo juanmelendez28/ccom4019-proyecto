0d05a19747c0f43a30a2f3e924b1d75527b9ec16
ricmozz8 <cupcakethief2311@gmail.com>
2024-12-15 18:41:58 -0400

    adding active term at enter page

--- views/enter_page.php
+++ views/enter_page.php
@@ -1,36 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php require_once 'partials/header.php'; ?>
+
 <body>
     <main>
         <?php require_once 'partials/navbar.php'; ?>
-            <table class="tblCourses">
-                <caption>Term Offering Courses</caption>
-                <thead>
+        <table class="tblCourses">
+            <caption>Current Avaliable Courses (<?= $currentTerm->term_id ?>)</caption>
+            <thead>
+                <tr>
+                    <th scope="col">Course name</th>
+                    <th scope="col">Course code</th>
+                    <th scope="col">Credits</th>
+                    <th scope="col">Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // rendering each course
+                foreach ($courses as $course) { ?>
                     <tr>
-                        <th scope="col">Course name</th>
-                        <th scope="col">Course code</th>
-                        <th scope="col">Credits</th>
-                        <th scope="col">Description</th>
+                        <td><?= $course->course_name ?></td>
+                        <td><span aria-describedby="<?= $course->course_id ?>_desc"><?= $course->course_id ?></span></td>
+                        <td><?= $course->course_credits ?></td>
+                        <?php
+                        $course_summary = substr($course->course_desc, 0, 100);
+                        if (strlen($course->course_desc) > 100) {
+                            $course_summary .= '...';
+                        }
+                        ?>
+                        <td><span id="<?= $course->course_code ?>_desc" class="course_desc" title="<?= $course->course_desc ?>"><?= $course_summary ?></span></td>
+                        <?php if (Auth::check() && (Auth::user()->role == 'admin' || Auth::user()->dept_id == $department->dept_id)) { ?>
+                            <td><a href="courses/edit/<?= $course->course_id ?>">Edit</a></td>
+                            <td><a href="courses/delete/<?= $course->course_id ?>">Delete</a></td>
+                        <?php } ?>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    // rendering each course
-                    foreach ($courses as $course) { ?>
-                        <tr>
-                            <td><?= $course->course_name ?></td>
-                            <td><span aria-describedby="<?= $course->course_id ?>_desc"><?= $course->course_id ?></span></td>
-                            <td><?= $course->course_credits ?></td>
-                            <!-- <td><span id="<?= $course->attributes['course_code'] ?>_desc" class="course_desc" title="<?= $course->course_desc ?>"><?= $course->course_desc ?></span></td>
-                            <?php if ($user->role == 'admin' || $user->dept_id == $department->dept_id) { ?>
-                                <td><a href="courses/edit/<?= $course->course_id ?>">Edit</a></td>
-                                <td><a href="courses/delete/<?= $course->course_id ?>">Delete</a></td> -->
-                            <?php } ?>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+                <?php } ?>
+            </tbody>
+        </table>
 
 
     </main>
