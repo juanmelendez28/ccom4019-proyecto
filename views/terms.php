92cf4888a8270e520824b044063af7e4691fdb5e
ricmozz8 <cupcakethief2311@gmail.com>
2024-12-16 19:01:07 -0400

    change icon to add course

--- views/terms.php
+++ views/terms.php
@@ -32,7 +32,7 @@
                             <td><a href="index.php?terms&edit=<?= $term->term_id ?>"><i class="las la-pen"></i>Edit</a></td>
                             <td><a href="index.php?terms&delete=<?= $term->term_id ?>"><i class="las la-trash"></i>Delete</a></td>
                             <td><a href="index.php?terms&activate=<?= $term->term_id ?>"><i class="las la-check-circle"></i>Activate</a></td>
-                            <td><a href="index.php?terms&add_course=<?= $term->term_id ?>"><i class="las la-check-circle"></i>Add course to term</a></td>
+                            <td><a href="index.php?terms&add_course=<?= $term->term_id ?>"><i class="las la-plus"></i>Add course to term</a></td>
                         <?php } ?>
                     </tr>
                 <?php } ?>
