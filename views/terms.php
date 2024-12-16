e8725ffceed03c37a8a9c274fa95e8169232763f
lian4melendez <lian.melendez@upr.edu>
2024-12-16 19:37:25 -0400

    .

--- views/terms.php
+++ views/terms.php
@@ -34,8 +34,8 @@
                             <td><a href="index.php?terms&edit=<?= $term->term_id ?>"><i class="las la-pen"></i>Edit</a></td>
                             <td><a href="index.php?terms&delete=<?= $term->term_id ?>"><i class="las la-trash"></i>Delete</a></td>
                             <td><a href="index.php?terms&activate=<?= $term->term_id ?>"><i class="las la-check-circle"></i>Activate</a></td>
+                            <?php } ?>
                             <td><a href="index.php?terms&add_course=<?= $term->term_id ?>"><i class="las la-plus"></i>Add course to term</a></td>
-                        <?php } ?>
                     </tr>
                 <?php } ?>
             </tbody>
