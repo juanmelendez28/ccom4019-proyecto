4f91bc2a243d8fdd432b62c33f4b09723eeeee33
juanmelendez28 <118770773+juanmelendez28@users.noreply.github.com>
2024-12-16 22:31:50 -0400

    comments on department controller

--- views/department_delete.php
+++ views/department_delete.php
@@ -3,40 +3,20 @@
 <?php require_once 'partials/header.php'; ?>
 
 <body>
-    <?php if ($_SERVER['REQUEST_METHOD'] === 'GET') { ?>
-        <form class="form" action="#" method="post">
-            <h2>Are you sure you want to delete this department?</h2>
-            <input disabled type="hidden" name="id" value="<?= $department->dept_id ?>">
-            <label for="name">Department name</label>
-            <input disabled type="text" name="name" value="<?= $department->dept_name ?>">
-            <label for="code">Department code</label>
-            <input disabled type="text" name="code" value="<?= $department->dept_id ?>">
-            <div class="action-group">
-                <input type="submit" class="action danger" value="Delete">
-                <div class="loader"></div>
-            </div>
+    <form class="form" action="#" method="post">
+        <h2>Are you sure you want to delete this department?</h2>
+        <input disabled type="hidden" name="id" value="<?= $department->dept_id ?>">
+        <label for="name">Department name</label>
+        <input disabled type="text" name="name" value="<?= $department->dept_name ?>">
+        <label for="code">Department code</label>
+        <input disabled type="text" name="code" value="<?= $department->dept_id ?>">
+        <div class="action-group">
+            <input type="submit" class="action danger" value="Delete">
+            <div class="loader"></div>
+        </div>
 
 
-        </form>
-
-    <?php
-
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-        $department_id = filter_input(INPUT_POST, 'id', FILTER_DEFAULT);
-
-        if(sizeof($department->courses()) > 0){
-            $_SESSION['error'] = 'Cannot delete department with courses';
-            redirect('?departments');
-        }
-
-        $success = $department->delete();
-
-        $success ? $_SESSION['success'] = 'Department deleted successfully' : $_SESSION['error'] = 'Failed to delete department';
-        $departments = Department::all();
-        redirect('?departments');
-    }
-    ?>
+    </form>
 
 
     <?php if (isset($_SESSION['error'])): ?>
